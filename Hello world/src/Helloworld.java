d9e2a80b9191ab83062c22ca9ae056386a6c0706
abalas4 <sagar.balu@gmail.com>
2017-04-15 12:01:11 +0530

    First commit from Eclipse for Selenium Demo

---
+++ Hello world/src/Helloworld.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+public class Helloworld 
+{
+
+    public static void main(String[] args) 
+    {
+        // declaration and instantiation of objects/variables
+    	WebDriver driver ;
+    	System.setProperty("webdriver.chrome.driver","C:\\webdrivers\\chromedriver.exe");
+    	driver = new ChromeDriver();
+        String baseUrl = "http://localhost:5244/about";
+        String expectedTitle = "about page";
+        String actualTitle = "";
+
+        // launch Fire fox and direct it to the Base URL
+        driver.get(baseUrl);
+
+        // get the actual value of the title
+        actualTitle = driver.getTitle();
+
+        /*
+         * compare the actual title of the page with the expected one and print
+         * the result as "Passed" or "Failed"
+         */
+        assert expectedTitle == actualTitle;
+       
+        System.out.println(actualTitle);
+        //close Fire fox
+        driver.close();
+       
+        // exit the program explicitly
+        System.exit(0);
+    }
+
+}
